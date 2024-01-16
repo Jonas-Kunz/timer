c3e322a43c79b7033c73f3d4ced0e3079eb15fb4
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-16 13:25:12 -0400

    commented

--- timer1.js
+++ timer1.js
@@ -3,12 +3,17 @@
 
 
 const timer1 = function() {
+  // takes command line args
   const timesArr = process.argv.slice(2);
+  // beeps
   let beep = '\x07';
-
+  /// loops through array
   for (let elem in timesArr) {
+    // executes if input is greater than 0 and a number
     if (elem > 0 && elem !== NaN) {
+      // turns 1 into 1000 converts s to ms
       let time = Number(timesArr[elem]) * 1000;
+      //timer
       setTimeout(() => {
         process.stdout.write(`\r${beep}`);
       }, time);
@@ -17,5 +22,5 @@ const timer1 = function() {
   };
 
 };
-
+// calls it:
 timer1();
